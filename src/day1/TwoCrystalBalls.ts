e3e0a55a25f29569c8dd4e9de998bc836ea7e439
Jamil Sinno <jamil.sinno@gmail.com>
2023-04-28 22:37:27 -0300

    solved crystal ball problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    let jumpDist = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jumpDist;
+    for (; i < breaks.length; i += jumpDist) {
+        if (breaks[i]){
+            break;
+        }
+    }
+
+    i -= jumpDist;
+
+    for (let j = 0; j < jumpDist && i < breaks.length; ++j, ++i) {
+        if (breaks[i]){
+            return i;
+        }
+    }
+    return -1;
 }
