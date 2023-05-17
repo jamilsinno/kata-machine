e1b8008cecc2019f958bd507e2f42c440e867f3f
Jamil Sinno <jamil.sinno@gmail.com>
2023-05-16 21:17:25 -0300

    quick sort and doubly linked lists

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,35 @@
-export default function quick_sort(arr: number[]): void {
+function qs (arr: number[], lo: number, hi: number){
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivotInd = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotInd - 1);
+    qs(arr, pivotInd + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number) : number {
+    const pivot = arr[hi];
+
+    let index = lo - 1;
+
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] < pivot) {
+            index++;
+            const temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+    }
+
+    index++;
+    arr[hi] = arr[index];
+    arr[index] = pivot;
+
+    return index;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
